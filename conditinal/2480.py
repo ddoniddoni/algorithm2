60f1d10ffa58818d210cd505d8891c2d25bae628
tony <tony@playzoey.com>
2025-05-30 02:58:31 +0900

    [조건문] 2480 수정

--- conditinal/2480.py
+++ conditinal/2480.py
@@ -1,2 +1,16 @@
 x, y, z = map(int, input().split())
-print(x,y,z)
+reward = 0
+
+if x == y == z:
+    reward = 10000 + (x * 1000)
+elif x == y or y == z or x == z:
+    if x == y:
+        reward = 1000 + x * 100
+    elif y == z:
+        reward = 1000 + y * 100
+    else:
+        reward = 1000 + z * 100
+else:
+    reward = max(x,y,z) * 100
+
+print(reward)
